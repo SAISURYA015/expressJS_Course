ba3ffb49fb19757405bb15353fc3c1f9a52bd773
SAISURYA <saisurya015@gmail.com>
2025-04-16 12:30:27 +0530

    middleware

--- index.js
+++ index.js
@@ -2,35 +2,50 @@ import express from "express";
 import router from "./routes.js";
 
 const app = express()
-app.use(express.json())
+// app.use(express.json())
 
-const PORT = 3000
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Express');
+app.use('/welcome', (req, res, next) => {
+  console.log('A new request recevied' + Date.now())
+  next()
 })
 
-app.post('/user', (req, res) => {
-  const { name, email } = req.body;
-  res.json({
-    message: `User ${name} with email ${email}`
-  })
+app.get('/error', (req, res) => {
+  throw new Error('this is an Error') 
 })
 
-app.put('/user/:id', (req, res) => {
-  const userId = req.params.id;
-  const { name, email } = req.body;
-  res.json({
-    message: `User ${userId} updated to ${name} with email ${email}`
-  })
+app.use((err, req, res, next)=>{
+  console.error(err.message);
+  res.send('internal server Error')
+  
 })
 
-app.delete('/user/:id', (req, res) => {
-  const userId = req.params.id
-  res.json({
-    message: `User ${userId} deleted successfully`
-  })
-})
+// app.get('/welcome', (req, res) => {
+//   res.send('Welcome to express app');
+// })
+
+// app.post('/user', (req, res) => {
+//   const { name, email } = req.body;
+//   res.json({
+//     message: `User ${name} with email ${email}`
+//   })
+// })
+
+// app.put('/user/:id', (req, res) => {
+//   const userId = req.params.id;
+//   const { name, email } = req.body;
+//   res.json({
+//     message: `User ${userId} updated to ${name} with email ${email}`
+//   })
+// })
+
+// app.delete('/user/:id', (req, res) => {
+//   const userId = req.params.id
+//   res.json({
+//     message: `User ${userId} deleted successfully`
+//   })
+// })
 
 app.use('/user', router)
 
