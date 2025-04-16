286d4ea23996b40af7e14822c9e3742e98ccaa47
SAISURYA <saisurya015@gmail.com>
2025-04-16 11:58:26 +0530

    rouets&searchparam

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require("express")
+import express from "express";
 
 const app = express()
 
 const PORT = 3000
 
-app.get('/', (req, res)=>{
-  res.send('Hello Express')
+app.get('/', (req, res) => {
+  res.send('Hello Express');
 })
 
-app.listen(PORT, ()=>{
+app.get('/user/:username', (req, res) => {
+  const username = req.params.username;
+  res.send(`Welcome ${username}`)
+})
+
+app.get('/search', (req, res)=>{
+  const keyword = req.query.keyword;
+  res.send(`Searching	for ${keyword}`)
+})
+
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
